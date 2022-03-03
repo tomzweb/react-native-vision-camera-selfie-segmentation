4d3f9eda6218c8f3beac71981097a2a248613e74
Tom Saunders <tsaunders83@gmail.com>
2022-03-03 13:04:49 +0000

    Initial biolerplate and version-camera install

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,17 +2,31 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
 import { multiply } from 'react-native-vision-camera-selfie-segmentation';
+import {Camera, useCameraDevices} from "react-native-vision-camera";
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
+  const devices = useCameraDevices();
+  const device = devices.back
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    const permissions = async () => {
+      const newCameraPermission = await Camera.requestCameraPermission()
+      const newMicrophonePermission = await Camera.requestMicrophonePermission()
+    }
+
+    permissions();
   }, []);
+
+  if (device == null) return <></>
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+      />
     </View>
   );
 }
